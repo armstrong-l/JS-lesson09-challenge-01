237994df5fc8997046104c2a99993c620441160d
linda_a <larmstrongnz@gmail.com>
2024-09-28 17:49:25 +1200

    Added dishes assignment to guests in JS

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
 addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
     // console.log(guest);
@@ -45,3 +48,25 @@ const updateGuestCount = function() {
         guestFull.classList.remove("hide");
     };
 };
+
+const assignItems = function () { 
+    const potluckItems = ["garden salad", "potato salad", "chicken drumsticks", "sausages", "beef steaks", "hummus", "chips", "cheese", "crackers", "fruit", "cookies", "icecream"];
+    const allGuests = document.querySelectorAll(".guest-list li");
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}`;
+        assignedItems.append(listItem);
+        potluckItems.splice(randomPotluckIndex, 1);
+    }
+    };
+
+    assignButton.addEventListener("click", function() {
+        assignItems();
+        assignButton.disabled = true;
+    })
+
+
+
+    
